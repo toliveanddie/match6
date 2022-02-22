7ab5c8e86f44d601fb6de8b7b2bb9cacf0793e2b
norm <toliveanddie76@gmail.com>
2022-02-21 20:53:20 -0500

    add pick5

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -179,4 +179,50 @@ module WelcomeHelper
 
   end
 
+  def pick5
+    doc = Nokogiri::HTML(URI.open('https://www.lotteryusa.com/pennsylvania/pick-5/year'))
+    results = []
+    doc.css('li').each do |data|
+      d = data.content.strip
+      if d.length == 1
+        if d.to_i < 10
+          results.push(d)
+        end
+      end
+    end
+
+    draws = []
+
+    50.times do |i|
+      draws.push(results.shift(5))
+    end
+
+    pool0 = Hash.new(0)
+    pool1 = Hash.new(0)
+    pool2 = Hash.new(0)
+    pool3 = Hash.new(0)
+    pool4 = Hash.new(0)
+    pools = Hash.new(0)
+
+    pool_array = [pool0, pool1, pool2, pool3, pool4]
+    
+    pool_array.length.times do |j|
+      draws.each do |draw|
+        pool_array[j][draw[j]] = pool_array[j][draw[j]] + 1
+        break if pool_array[j].size >= 10
+      end
+    end
+
+    pool_array.each do |pool|
+      pools[pool] = pool.values.sum
+    end
+
+    pools.each do |pool, total|
+      pool.delete_if {|k,v| v < pool.values.sort.last(2).first }
+    end
+
+    return pools
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/powerball', to: 'welcome#powerball'
   get '/pick4', to: 'welcome#pick4'
+  get '/pick5', to: 'welcome#pick5'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
